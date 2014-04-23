316a17328e5cc2dbac094e8f4130b5347b5d0013
Stephen Gardner <opensourceaugie@gmail.com>
2014-04-23 04:50:52 -0400

    github.php finalized

--- github.php
+++ github.php
@@ -2,7 +2,7 @@
 	// a php file that executes the shell_exec function (by using backticks),
 	// this URL, when pinged, will execute a git pull, updating our repo every time this project is pushed to github
 	echo ("pulling git<br/>");
-	$output = shell_exec("dir");
+	$output = shell_exec("git pull");
 	echo("output is: <br/>");
 	print_r($output);
 ?>
