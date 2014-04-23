26bb250ecb902f7eea296a710d2e81886a51ef52
Stephen Gardner <opensourceaugie@gmail.com>
2014-04-23 03:57:03 -0400

    testing pulling on commit

--- github.php
+++ github.php
@@ -1,5 +1,6 @@
 <?php
 	// a php file that executes the shell_exec function (by using backticks),
 	// this URL, when pinged, will execute a git pull, updating our repo every time this project is pushed to github
+	echo ("pulling git");
 	`git pull`;
 ?>
