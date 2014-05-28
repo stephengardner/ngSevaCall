303f32e334f662f113cd4a57bf8a5d99e878c5a2
stephengardner <augdog911@gmail.com>
2014-05-28 16:27:00 -0400

    ios styles

--- mobile/js/controllers.js
+++ mobile/js/controllers.js
@@ -31,7 +31,7 @@ angular.module('myApp.controllers', [])
         });
     }])
     .controller('bodyController', ['$rootScope', 'Request', 'Menu', '$attrs', '$scope', '$location', function($rootScope, Request, Menu, $attrs, $scope, $location){
-        $location.path("/recording");
+        $location.path("/step1");
         $scope.menu = Menu;
         $scope.click = function($event) {
             Menu.active = false;
