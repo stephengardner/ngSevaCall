df2880d78d0fa132235e92ec059a5854db903c12
stephengardner <opensourceaugie@gmail.com>
2014-09-08 18:21:42 -0400

    title and fb app updated

--- app/scripts/controllers/controllers.js
+++ app/scripts/controllers/controllers.js
@@ -497,13 +497,13 @@ angular.module('myApp.controllers', [])
 	        Track.event(2, 'facebook_share_button_pressed', true);
             var url = "http://facebook.com/dialog/feed";
             url += "?app_id=696501850437286";
-            url += "&link=www.talklocal.com";
+            url += "&link=http://talklocal.com";
             url += "&name= Seva Call - Tell us what and when, we'll find the professionals!";
             url += "&description=talklocal works to find local businesses that can help you with your service need instantly, like a free personal concierge service. Within minutes you will be connected to providers that can service your specific problem, on your schedule, at your location.";
             url += "&redirect_uri=http://talklocal.com";
             console.log("popping up fb url: " + url);
-            window.open("http://www.facebook.com/dialog/feed?app_id=696501850437286&link=http://www.talklocal.com&name="
-                + encodeURIComponent("talklocal - Tell us what and when, we'll find the professionals!") + "&redirect_uri=http://sevacall.com", '_system');
+            window.open("http://www.facebook.com/dialog/feed?app_id=696501850437286&link=http://talklocal.com&name="
+                + encodeURIComponent("talklocal - Tell us what and when, we'll find the professionals!") + "&redirect_uri=http://talklocal.com", '_system');
             return false;
         }
     }])
